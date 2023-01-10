b5bea18cf5f76f5d32ebfffd1d6a079a92fed392
Richard Hoyle <richardhoyle900@gmail.com>
2023-01-10 14:55:45 +0000

    functionality to render all articles in card tiles

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <>
+            <h1 id="Site Name">My Boring News Website</h1>
+            <h2 id="Website tag line">For all your news needs... </h2>
+      </>
+    );
+}
+
+export default Header
