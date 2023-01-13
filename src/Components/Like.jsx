0b357f6b926b4d03d3f0e0afced68d315b721df2
Richard Hoyle <richardhoyle900@gmail.com>
2023-01-13 13:51:34 +0000

    added a downvote button

--- src/Components/Like.jsx
+++ src/Components/Like.jsx
@@ -3,15 +3,25 @@ import { likeArticleById } from "../utils/api";
 
 const Like = ({ likes, articleId }) => {
   const [likeChange, setLikeChange] = useState(0);
-  const [isError, setIsError] = useState (null);
+  const [isError, setIsError] = useState(null);
 
-  const handleLike = () => {
-    setIsError(null)
-    setLikeChange((currLikeChange) => currLikeChange + 1);
-    likeArticleById(articleId, 1).catch((err) => {
+  const handleLike = ({ currentTarget }) => {
+    const dataValue = currentTarget.getAttribute("data-value");
+    console.log(dataValue);
+    setIsError(null);
+    if (!dataValue) {
+      setLikeChange((currLikeChange) => currLikeChange + 1);
+      likeArticleById(articleId, 1).catch((err) => {
+        setLikeChange((currLikeChange) => currLikeChange - 1);
+        setIsError("Whoops, something went wrong...try again?");
+      });
+    } else {
       setLikeChange((currLikeChange) => currLikeChange - 1);
-      setIsError("Whoops, something went wrong...try again?")
-    });
+      likeArticleById(articleId, -1).catch((err) => {
+        setLikeChange((currLikeChange) => currLikeChange + 1);
+        setIsError("Whoops, something went wrong...try again?");
+      });
+    }
   };
 
   return (
@@ -19,6 +29,9 @@ const Like = ({ likes, articleId }) => {
       <p>Votes: {likes + likeChange}</p>
       <button onClick={handleLike} className="upvoter">
         👍
+      </button>
+      <button data-value="dislike" onClick={handleLike} className="upvoter">
+        👎
       </button>
       <p className="vote-err">{isError}</p>
     </section>
