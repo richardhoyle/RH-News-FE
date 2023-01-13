a4d4f03d671e577e54f723c3affaf3d62a985aeb
Richard Hoyle <richardhoyle900@gmail.com>
2023-01-13 12:02:39 +0000

    added a like/vote button on the article

---
+++ src/Components/Like.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { likeArticleById } from "../utils/api";
+
+const Like = ({ likes, articleId }) => {
+  const [likeChange, setLikeChange] = useState(0);
+  const [isError, setIsError] = useState (null);
+
+  const handleLike = () => {
+    setIsError(null)
+    setLikeChange((currLikeChange) => currLikeChange + 1);
+    likeArticleById(articleId, 1).catch((err) => {
+      console.log(err, '<err in like')
+      setLikeChange((currLikeChange) => currLikeChange - 1);
+      setIsError("Whoops, something went wrong...try again?")
+    });
+  };
+
+  return (
+    <section>
+      <p>Votes: {likes + likeChange}</p>
+      <button onClick={handleLike} className="upvoter">
+        👍
+      </button>
+      <p className="vote-err">{isError}</p>
+    </section>
+  );
+};
+
+export default Like;
